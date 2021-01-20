d4b08f1436a5906183abbe7626c9b472ada6549d
Aly <me@s5.pm>
2021-01-19 18:35:03 -0800

    Add flatMap to Monad

--- core/src/main/scala/Monad.scala
+++ core/src/main/scala/Monad.scala
@@ -5,4 +5,7 @@ trait Monad[Obj, Hom[_ <: Obj, _ <: Obj], F[_ <: Obj] <: Obj] extends Functor[Ob
   def unit[A <: Obj]: Hom[A, F[A]]
   def join[A <: Obj]: Hom[F[F[A]], F[A]]
 
+  def flatMap[A <: Obj, B <: Obj](f: Hom[A, F[B]])(implicit comp: Compose[Obj, Hom]): Hom[F[A], F[B]] =
+    comp.andThen(map(f), join[B])
+
 }
